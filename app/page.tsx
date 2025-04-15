dd5d843a4dc4381718b677333c2524f528839f51
Jitesh Singh <81912066+CodrJitesh@users.noreply.github.com>
2025-04-16 00:03:54 +0530

    Revert "Added Firebase Authentication (login and sign-Up)"

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import AuthForm from './components/AuthForm'
 
 const page = () => {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
-      <AuthForm />
-    </main>
+    <div>page</div>
   )
 }
 
