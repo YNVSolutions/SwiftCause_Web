657b7623eef9258ad9fe8bfe4c05aa56aa3ef880
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-17 23:56:59 +0530

    Creating the navigation menu

---
+++ app/Components/Others/NavBar.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+function Navbar() {
+  return (
+    <header className="bg-black py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold text-blue-500">
+          Swift Cause
+        </Link>
+        <nav className="space-x-6">
+          <Link href="/" className="text-white hover:text-blue-300">Features</Link>
+          <Link href="/" className="text-white hover:text-blue-300">Pricing</Link>
+          <Link href="/" className="text-white hover:text-blue-300">For Nonprofits</Link>
+          <Link href="/" className="text-white hover:text-blue-300">Developers</Link>
+          <Link href="/" className="text-white hover:text-blue-300">Contact</Link>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Start Fundraising
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
