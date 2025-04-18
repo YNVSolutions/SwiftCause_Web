deb48d915e34754ad1dd89bf86d25601be674ee5
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-18 18:42:49 +0530

    Relfecting the new components at main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NavBar from "./Components/Others/NavBar";
 import Main from "./Components/Others/Main";
+import Features from "./Components/Others/Features";
+import About from "./Components/Others/About";
 
 const Page = () => {
   return (
     <div>
       <NavBar />
       <Main />
+      <About/>
+      <Features />
     </div>
   );
 };
