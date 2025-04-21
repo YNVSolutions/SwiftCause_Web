6b17901b4cee05c431daae6d4dbea154599c9981
Jitesh Singh <jiteshSingh4305@gmail.com>
2025-04-22 02:27:48 +0530

    integrated firestore

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { app } from "../Components/Auth/firebase.tsx";
+import CampaignList from "../Campaign/CampaignList.jsx";
+
+const firestore = getFirestore(app);
+
+const Page = () => {
+  
+
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
+      <CampaignList />
+    </div>
+  );
+};
+
+export default Page;
