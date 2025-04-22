ba20d0efed08c0ba002f4bfb4f363717440012be
Jitesh Singh <jiteshSingh4305@gmail.com>
2025-04-23 01:40:04 +0530

    added donation amount graph

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,16 +3,18 @@ import React from "react";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { app } from "../Components/Auth/firebase.tsx";
 import CampaignList from "../Campaign/CampaignList.jsx";
+import AmountTrendChart from "../Campaign/AmountTrendChart.jsx";
 
 const firestore = getFirestore(app);
 
 const Page = () => {
-  
-
   return (
-    <div className="p-6 bg-gray-100">
+    <div className="p-6 bg-gray-200">
       <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
-      <CampaignList />
+      <div className="flex">
+        <AmountTrendChart campaignId="cmp_002" />
+        <CampaignList />
+      </div>
     </div>
   );
 };
