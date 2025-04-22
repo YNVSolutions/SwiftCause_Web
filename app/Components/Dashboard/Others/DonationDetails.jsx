a286aa2dd9d3537772e7d1fc07a56f3fc382ae48
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-22 22:01:50 +0530

    Create DonationDetails.jsx

---
+++ app/Components/Dashboard/Others/DonationDetails.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import DonationGraph from './DonationGraph'
+const DonationDetails = () => {
+    return (
+        <>
+            <div className='flex justify-between gap-3'>
+                <DonationGraph />
+                <div className='w-1/2 text-white mt-8'>
+                    <h1 className='text-3xl font-bold text-center'>The Donation Analytics</h1>
+                    <p className='font-medium mt-5 text-gray-300'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo incidunt fugiat laboriosam iusto amet. Repellendus perspiciatis ab cupiditate ducimus quos quis consequatur exercitationem architecto!</p>
+                    <div className='flex justify-center items-center gap-5'>
+                    <button className='bg-green-600 text-sm px-3 py-2 rounded'>Monthly</button><button className='bg-green-600 text-sm px-3 py-2 rounded'>Yearly
+                    </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DonationDetails
