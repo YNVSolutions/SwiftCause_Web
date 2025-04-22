82c0cfa937a5631e20b829fa9f585f2fd69cf2db
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-22 18:39:49 +0530

    create in usd

--- app/Components/Dashboard/Others/Details.jsx
+++ app/Components/Dashboard/Others/Details.jsx
@@ -5,7 +5,7 @@ const Details = () => {
         <>
             <div className='flex justify-between gap-5 screen py-10 '>
                 <div className='rounded-xl w-[45%] py-6 px-9 bg-gray-800 text-blue-400 text-center'>
-                    <h2 className='text-4xl font-bold '>₹12000+</h2>
+                    <h2 className='text-4xl font-bold '>$12000+</h2>
                     <h3 className='font-medium text-gray-400'>Total Donation</h3>
                 </div>
                 <div className='rounded-xl w-[45%] text-center py-6 px-9 bg-gray-800 text-green-400'>
