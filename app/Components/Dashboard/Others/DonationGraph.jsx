220f495a7ed52a14dcfbac1d8f3f4a7d572b5321
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-22 18:40:32 +0530

    Adding dummy data to graph

--- app/Components/Dashboard/Others/DonationGraph.jsx
+++ app/Components/Dashboard/Others/DonationGraph.jsx
@@ -1,8 +1,57 @@
+"use client";
 import React from 'react'
+import { Chart as ChartJS } from 'chart.js/auto';
+import { Bar ,Doughnut} from 'react-chartjs-2'
+import DonationData from './DonationData.json'
 
 const DonationGraph = () => {
+
   return (
     <>
+      <div className='w-3/4'>
+        <h2 className="text-center text-2xl font-bold mb-4">Donation Distribution</h2>
+        <Bar
+          data={{
+            labels: DonationData.map((data) => data.Orgination),
+            datasets: [
+              {
+                label: "Donation Amount",
+                data: DonationData.map((data) => data.Value),
+                borderWidth: 1,
+                backgroundColor:[
+                  "rgba(66, 165, 245, 1)",
+                  "rgba(102, 187, 106, 1)",
+                  "rgba(92, 107, 192, 1)"
+                ]
+              },
+            ],
+          }}
+          options={{
+            responsive: true,
+            plugins: {
+              title: {
+                display: true,
+                text: 'Donation Graph', 
+              },
+            },
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: 'Organizations', 
+                },
+              },
+              y: {
+                title: {
+                  display: true,
+                  text: 'Donation Value (in USD)',
+                },
+                beginAtZero: true,
+              },
+            },
+          }}
+        />
+      </div>
     </>
   )
 }
