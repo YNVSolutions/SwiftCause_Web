96afe47047b61e68a00dac26d07b0d0ef2c582e3
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-22 22:02:00 +0530

    adjusting the layout

--- app/Components/Dashboard/Others/DonationGraph.jsx
+++ app/Components/Dashboard/Others/DonationGraph.jsx
@@ -8,7 +8,7 @@ const DonationGraph = () => {
 
   return (
     <>
-      <div className='w-3/4'>
+      <div className='w-1/2'>
         <h2 className="text-center text-2xl font-bold mb-4">Donation Distribution</h2>
         <Bar
           data={{
