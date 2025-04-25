e95ade2b5b1a325e738b5105f2e4957691d102c5
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-25 11:10:40 +0530

    Update page.jsx

--- app/UserDashboard/page.jsx
+++ app/UserDashboard/page.jsx
@@ -4,13 +4,15 @@ import Header from './Others/Header'
 import Details from './Others/Details'
 import DonationList from './Others/DonationList'
 import DonationDetails from './Others/DonationDetails'
+import RewardsAndBadge from './UserComponents/RewardsAndBadges'
 const UserDashboard = () => {
     return (
         <>
-            <div className='min-h-screen'>
+            <div className='min-h-screen bg-black'>
                 <Header />
                 <Details />
                 <DonationList />
+                <RewardsAndBadge/>
                 <DonationDetails/>
             </div>
         </>
