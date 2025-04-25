0f0d4a9abffdc0c25ed21baf33ccbe0993917354
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-25 11:13:51 +0530

    updating the changes to the dashboard

--- app/UserDashboard/page.jsx
+++ app/UserDashboard/page.jsx
@@ -12,8 +12,13 @@ const UserDashboard = () => {
                 <Header />
                 <Details />
                 <DonationList />
-                <RewardsAndBadge/>
-                <DonationDetails/>
+                <div className='min-h-screen'>
+                    <div className='lg:text-5xl md:text-3xl sm:text-2xl text-white text-center font-semibold py-4'>
+                        <h1>Donation Analytics</h1>
+                    </div>
+                    <RewardsAndBadge />
+                    <DonationDetails />
+                </div>
             </div>
         </>
     )
