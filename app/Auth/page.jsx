6f2c144575e05262afd80d164bdbdab6af677a0d
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-26 22:36:35 +0530

    Separating the LoginIn page from components

---
+++ app/Auth/page.jsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useEffect } from "react";
+import { getAuth } from "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebaseui/dist/firebaseui.css";
+import { app } from "./firebase";
+import Image from "next/image";
+import NavBar from '../Components/NavBar'
+
+export default function Login() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      import("firebaseui").then((firebaseui) => {
+        const ui =
+          firebaseui.auth.AuthUI.getInstance() ||
+          new firebaseui.auth.AuthUI(getAuth(app));
+
+        ui.start("#firebaseui-auth-container", {
+          signInSuccessUrl: "/",
+          signInOptions: [
+            {
+              provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+              clientId:
+                "373490483164-o6ggs2k0c6i9a14mgfqtdhv6biucttti.apps.googleusercontent.com",
+            },
+            {
+              provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            },
+          ],
+          credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
+        });
+      });
+    }
+  }, []);
+  return (
+    <>
+      <NavBar />
+      <div className="flex items-center justify-center bg-black min-h-screen">
+        <div className="flex justify-center items-center">
+          <Image src="/head_image.png" width={400} height={400} alt="img" />
+        </div>
+        <div className="flex justify-center items-center  ">
+          <div className="bg-gray-900 backdrop-blur-md rounded-xl p-8 sm:p-10  border-b-2 border-t-2 border-blue-500 shadow-lg">
+            <h2 className="text-2xl sm:text-3xl font-semibold text-white mb-6 text-center">
+              LogIn 
+            </h2>
+            <div id="firebaseui-auth-container"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
