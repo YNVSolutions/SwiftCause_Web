f9dff707ba98ebee1e463aba7d4cbf32bf9ed2ba
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-26 22:37:58 +0530

    adding component to the folder

---
+++ app/Components/NavBar.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+function Navbar() {
+  return (
+    <header className="bg-black py-6 fixed w-full z-999">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold text-blue-500">
+          Swift Cause
+        </Link>
+        <nav className="space-x-6">
+          <Link href="/" className="text-white hover:text-blue-300">Pricing</Link>
+          <Link href="/Auth" className="text-white hover:text-blue-300">SignIn</Link>
+          <Link href="/UserDashboard" className="text-red-500 hover:text-blue-300">User-Dashboard</Link>
+          <Link href="/AdminDashboard" className="text-red-500 hover:text-blue-300">Admin-Dashboard</Link>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Start Fundraising
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
