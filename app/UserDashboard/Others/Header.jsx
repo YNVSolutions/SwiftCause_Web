102ecf4c56a8f8b86fb2a8b18518eabe94f59d2e
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-03 14:30:57 +0530

    adding padding layout

--- app/UserDashboard/Others/Header.jsx
+++ app/UserDashboard/Others/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-            <div className='flex flex-col md:flex-row items-center md:items-end justify-between text-white px-5 pt-6'>
+        
+            <div className='pt-20 flex flex-col md:flex-row items-center md:items-end justify-between text-white px-5'>
                 <h1 className='text-center md:text-left text-xl font-medium'>
                     Hello<br />
                     <span className='text-3xl font-semibold'>Yash Raghuvanshi 👋</span>
                 </h1>
                 <div className='flex gap-4 mt-4 md:mt-0'>
-                    <button className='bg-blue-600 hover:bg-blue-700 rounded-lg px-5 py-3 text-white'>Main</button>
                     <button className='bg-red-600 hover:bg-red-700 rounded-lg px-5 py-3 text-white'>Log Out</button>
                 </div>
             </div>
