ad54f0d84f5dcec9f341849606f3adc1d8870812
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-03 13:53:47 +0530

    margin adjustment

--- app/UserDashboard/Others/Details.jsx
+++ app/UserDashboard/Others/Details.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Details = () => {
     return (
         <>
-            <div className='flex  justify-center md:justify-between gap-5 py-10 overflow-x-auto'>
+            <div className='flex  justify-center md:justify-around gap-5 py-10 sm:flex-row flex-col mx-5'>
                 <div className='rounded-xl w-full md:w-[45%] py-6 px-9 bg-gray-800 text-blue-400 text-center'>
                     <h2 className='text-4xl font-bold'>$12000+</h2>
                     <h3 className='font-medium text-gray-400'>Total Donation</h3>
