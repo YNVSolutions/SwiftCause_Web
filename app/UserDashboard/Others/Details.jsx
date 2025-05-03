09a5754c193b6d125f1a6fa4ca7cd784b64e0400
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-03 13:50:28 +0530

    adding some layout adjustments

--- app/UserDashboard/Others/Details.jsx
+++ app/UserDashboard/Others/Details.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Details = () => {
     return (
         <>
-            <div className='flex justify-between gap-5 screen py-10 '>
-                <div className='rounded-xl w-[45%] py-6 px-9 bg-gray-800 text-blue-400 text-center'>
-                    <h2 className='text-4xl font-bold '>$12000+</h2>
+            <div className='flex  justify-center md:justify-between gap-5 py-10 overflow-x-auto'>
+                <div className='rounded-xl w-full md:w-[45%] py-6 px-9 bg-gray-800 text-blue-400 text-center'>
+                    <h2 className='text-4xl font-bold'>$12000+</h2>
                     <h3 className='font-medium text-gray-400'>Total Donation</h3>
                 </div>
-                <div className='rounded-xl w-[45%] text-center py-6 px-9 bg-gray-800 text-green-400'>
+                <div className='rounded-xl w-full md:w-[45%] py-6 px-9 bg-gray-800 text-green-400 text-center'>
                     <h2 className='text-4xl font-bold'>30+</h2>
-                    <h3 className='font-medium text-gray-400'>Donated Orginations</h3>
+                    <h3 className='font-medium text-gray-400'>Donated Organizations</h3>
                 </div>
-                <div className='rounded-xl w-[45%] text-center py-6 px-9 bg-gray-800 text-indigo-400'>
+                <div className='rounded-xl w-full md:w-[45%] py-6 px-9 bg-gray-800 text-indigo-400 text-center'>
                     <h2 className='text-4xl font-bold'>20+</h2>
                     <h3 className='font-medium text-gray-400'>Liked Campaign</h3>
                 </div>
