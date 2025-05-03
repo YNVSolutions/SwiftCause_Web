89e9ed2bebf459bfd5a1c85f354ee02f23032d59
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-03 14:02:25 +0530

    addin gy axis scroll

--- app/UserDashboard/Others/DonationList.jsx
+++ app/UserDashboard/Others/DonationList.jsx
@@ -6,7 +6,11 @@ const DonationList = () => {
             <header className='flex justify-center items-center w-full pt-5'>
                 <h1 className='text-4xl lg:text-4xl md:text-3xl sm:text-2xl font-semibold text-white'>Donation History</h1>
             </header>
-            <div id='tasklist' className='h-auto md:h-[55%] flex flex-wrap md:flex-nowrap items-center justify-center md:justify-start gap-5 py-5 mt-10 overflow-x-auto text-gray-100'>
+            <div
+                id='tasklist'
+                className='h-auto md:h-[55%] flex flex-col md:flex-row items-center justify-center md:justify-start gap-5 py-5 mt-10 overflow-y-auto md:overflow-x-auto text-gray-100 mx-5 scrollbar-hidden'
+                style={{ maxHeight: 'calc(3 * 150px + 6rem)' }} 
+            >
                 <div className='flex-shrink-0 w-full md:w-[300px] bg-gray-800 rounded-xl py-5 px-5'>
                     <div className='flex justify-between items-center'>
                         <h2 className='bg-green-600 text-sm px-3 py-1 rounded'>$2200</h2>
