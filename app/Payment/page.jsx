a474f2e150f721928e74fb1daa84c6e5aaa29961
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-11 19:30:44 +0530

    adding payment gateway

--- app/Payment/page.jsx
+++ app/Payment/page.jsx
@@ -1,24 +1,39 @@
 "use client";
 import React from 'react';
-//import CheckoutPage from '@/components/CheckoutPage'; 
-//import convertToSubcurrency from '@/lib/convertToSubcurrency';
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import CheckoutPage from '../Components/CheckoutPage'; 
+import convertToSubcurrency from '../lib/convertToSubcurrency';
 import PaymentHeader from './Others/PaymentHeader';
 import NavBar from '../Components/NavBar';
 
-//if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-//  throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined');
-//}
+if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+  throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined');
+}
 
-//const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 const Page = () => {
+  const amount = 1000;
   return (
     <>
-    <div className="fixed top-0 left-0 w-full z-50 bg-black">
+      <div className="fixed top-0 left-0 w-full z-50 bg-black">
         <NavBar />
       </div>
-      <div className="pt-20 flex flex-col md:flex-row h-screen bg-black text-white overflow-hidden">
+      <div className="pt-24">
+        <div>
+          <PaymentHeader />
+        </div>
+        <Elements 
+          stripe={stripePromise}
+          options={{
+            mode: 'payment',
+            amount: convertToSubcurrency(amount),
+            currency: 'usd',
+          }}
+        >
+          <CheckoutPage amount={amount} />
+        </Elements>
       </div>
     </>
   );
