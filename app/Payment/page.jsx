97236cfcba2ecd6cf1139f94ac7f68794e487e2e
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-12 11:47:42 +0530

    adding layout styling to the page

--- app/Payment/page.jsx
+++ app/Payment/page.jsx
@@ -2,10 +2,11 @@
 import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import CheckoutPage from '../Components/CheckoutPage'; 
-import convertToSubcurrency from '../lib/convertToSubcurrency';
+import CheckoutPage from './Others/CheckoutPage';
+import convertToSubcurrency from './lib/convertToSubcurrency';
 import PaymentHeader from './Others/PaymentHeader';
 import NavBar from '../Components/NavBar';
+import Image from 'next/image';
 
 if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
   throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined');
@@ -13,7 +14,7 @@ if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
-const Page = () => {
+const Payment = () => {
   const amount = 1000;
   return (
     <>
@@ -24,19 +25,26 @@ const Page = () => {
         <div>
           <PaymentHeader />
         </div>
-        <Elements 
-          stripe={stripePromise}
-          options={{
-            mode: 'payment',
-            amount: convertToSubcurrency(amount),
-            currency: 'usd',
-          }}
-        >
-          <CheckoutPage amount={amount} />
-        </Elements>
+        <div className="flex mt-7">
+        <div className='w-1/2'>
+          <Image src="/head_image.png" width={800} height={800} alt='donate'/>
+        </div>
+        <div  className='w-1/2'>
+          <Elements
+            stripe={stripePromise}
+            options={{
+              mode: 'payment',
+              amount: convertToSubcurrency(amount),
+              currency: 'usd',
+            }}
+          >
+            <CheckoutPage amount={amount} />
+          </Elements>
+        </div>
+        </div>
       </div>
     </>
   );
 };
 
-export default Page;
+export default Payment;
