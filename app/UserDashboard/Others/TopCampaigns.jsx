4c259c5499b9122408f2a0ceab006bf47d1f5ad8
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-05-17 12:27:35 +0530

    integrating the firebase real time data

--- app/UserDashboard/Others/TopCampaigns.jsx
+++ app/UserDashboard/Others/TopCampaigns.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
-import Image from 'next/image'
-const TopCampaigns = () => {
-    const topCampaignsData = [
-        { id: 1, name: 'Stand-alone global analyzer', raised: 39947, image: '/demo1.png' },
-        { id: 2, name: 'Innovative value-added toolset', raised: 36646, image: '/demo2.png' },
-        { id: 3, name: 'Upgradable content-based hierarchy', raised: 36068, image: '/demo3.png' },
-    ];
-    const formatCurrency = (amount) => `£${amount.toLocaleString()}`;
+
+const TopCampaigns = ({ data }) => {
     return (
         <>
             <div className="bg-gray-800 rounded-lg p-6 shadow-md">
                 <h2 className="text-lg font-semibold text-gray-400 mb-3">Top Campaigns</h2>
-                <ul className="space-y-3">
-                    {topCampaignsData.map((campaign) => (
-                        <li key={campaign.id} className="flex items-center space-x-3">
+                <ul className="space-y-3 overflow-y-auto max-h-[150px]">
+                    {data.map((campaign) => (
+                        <li key={campaign.id} className="flex items-center space-x-3 ">
                             <div className="relative w-8 h-8 rounded-full overflow-hidden">
-                                {campaign.image && (
-                                    <Image src={campaign.image} alt={campaign.name} layout="fill" objectFit="cover" />
-                                )}   
+                               {/* image calling from firebase storage
+                                <img
+                                    src={campaign.coverImageUrl}
+                                    alt={campaign.title}
+                                    className="object-cover w-8 h-8"
+                                />*/}
+                                <img
+                                    src='/demo2.png'
+                                    alt={campaign.title}
+                                    className="object-cover w-8 h-8"
+                                />
                             </div>
                             <div className="flex-grow">
-                                <h3 className="text-sm font-semibold">{campaign.name}</h3>
-                                <p className="text-xs text-teal-400">{formatCurrency(campaign.raised)}</p>
+                                <h3 className="text-sm font-semibold">{campaign.title}</h3>
+                                <p className="text-xs text-teal-400">£{campaign.collectedAmount}</p>
                             </div>
                         </li>
                     ))}
