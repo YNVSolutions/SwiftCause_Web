b4e2fd15879b20a54bc05d0b477337ea227163b7
Jitesh Singh <jiteshsingh4305@gmail.com>
2025-08-11 22:53:58 +0530

    Added faker and seeded the data

--- seedKiosk.js
+++ seedKiosk.js
@@ -4,14 +4,6 @@ import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 import { faker } from '@faker-js/faker';
 
 // --- 1. Your Web SDK Firebase config ---
-const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
 
 // --- 2. Initialize Firebase ---
 const app = initializeApp(firebaseConfig);
