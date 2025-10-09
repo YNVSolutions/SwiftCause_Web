d1f9bae9eeaf8172ebab9c82e30a21bc3eecdcc2
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-10-09 18:37:27 +0530

    Firebase libs

---
+++ src/shared/lib/currencyFormatter.ts
@@ -0,0 +1,7 @@
+export const formatCurrency = (amount: number, currency: string = 'USD') => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency,
+    minimumFractionDigits: 2
+  }).format(amount);
+};
---
+++ src/shared/lib/index.ts
@@ -0,0 +1,4 @@
+// Libraries and utilities
+export * from './firebase';
+export * from './imageUpload';
+export * from './currencyFormatter';
