4b45b2e25a19e61b7a85e89042ebffa2e2a7fa42
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-10-15 19:26:32 +0530

    fixing the campaign component paths

--- src/features/campaigns/hooks/useCampaignList.ts
+++ src/features/campaigns/hooks/useCampaignList.ts
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
-import { useCampaigns } from '../../../shared/lib/hooks/useCampaigns';
-import { Campaign, KioskSession } from '../../../shared/types';
+import { useCampaigns, Campaign } from '../../../entities/campaign';
+import { KioskSession } from '../../../shared/types';
 
 export interface UseCampaignListReturn {
   campaigns: Campaign[];
