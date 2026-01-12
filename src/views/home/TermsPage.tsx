463387503a34c4c1b5811f8a43931159d4fe7fef
Yash Raghuvanshi <yashraghuvans@gmail.com>
2026-01-13 01:59:49 +0530

    fixes bug

--- src/views/home/TermsPage.tsx
+++ src/views/home/TermsPage.tsx
@@ -30,7 +30,7 @@ export function TermsPage({ onNavigate }: { onNavigate?: (screen: string) => voi
           <div className="flex items-center space-x-3">
             <div className="h-8 w-8 sm:h-10 sm:w-10">
               <img 
-                src={swiftCauseLogo} 
+                src={swiftCauseLogo.src} 
                 alt="Swift Cause Logo" 
                 className="w-full h-full object-contain"
               />
