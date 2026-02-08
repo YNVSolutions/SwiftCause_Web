0e1fd6415513d13eeac008a6e2ba843fba729026
Yash Raghuvanshi <yashraghuvans@gmail.com>
2026-02-09 00:56:24 +0530

    Update index.ts

--- src/views/home/components/index.ts
+++ src/views/home/components/index.ts
@@ -1,1 +1,5 @@
 export { AnimatedDashboardDemo } from './AnimatedDashboardDemo';
+export { Navbar } from './Navbar';
+export { HeroSection } from './HeroSection';
+export { FeaturesSection } from './FeaturesSection';
+export { DemoSection } from './DemoSection';
